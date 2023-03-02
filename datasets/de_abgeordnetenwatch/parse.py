375364c5ffa3a7e10d266c6e8655deb578df59b4
Philipp Michael <michael@abgeordnetenwatch.de>
2023-03-02 15:12:09 +0100

    cleanup: remove unused functions and replace single quotes
    (cherry picked from commit fb4df9dca8d73511d64f2004a6c7737ac116070f)

--- datasets/de_abgeordnetenwatch/parse.py
+++ datasets/de_abgeordnetenwatch/parse.py
@@ -64,7 +64,6 @@ def make_membership(
     rel.add("summary", additionalInformation)
     return rel
 
-# TODO: This is copied from lobbyregister. Adjust if needed or remove.
 def make_address(context: Zavod, data) -> CE:
     proxy = context.make("Address")
     city = data["field_city"].pop("label")
@@ -96,11 +95,12 @@ def make_person(context: Zavod, org_ident: str, data: dict[str, Any]) -> CE:
     proxy.id = context.make_slug("person", ident)
     return proxy
 
+
 def make_organization(context: Zavod, data: dict[str, Any]) -> CE:
     proxy = context.make("Organization")
     proxy.add("name", data.pop("label"))
 
-    if (data['field_country']):
+    if (data["field_country"]):
         addr = make_address(context, data)
         context.emit(addr)
         proxy.add("addressEntity", addr)
@@ -114,76 +114,11 @@ def make_organization(context: Zavod, data: dict[str, Any]) -> CE:
     return proxy
 
 
-# TODO: This is copied from lobbyregister. Adjust if needed or remove.
-def make_representation(
-    context: Zavod, agent: CE, client: CE, role: str | None = "Auftraggeber"
-) -> CE:
-    rel = context.make("Representation")
-    ident = make_entity_id(client.id, agent.id)
-    rel.id = context.make_slug("representation", ident)
-    rel.add("client", client)
-    rel.add("agent", agent)
-    rel.add("role", role)
-    return rel
-
-
-# TODO: This is copied from lobbyregister. Adjust if needed or remove.
-def wrangle_organization(context: Zavod, proxy: CE, data: dict[str, Any]) -> CE:
-    proxy.add("name", data.pop("name"))
-    proxy.add("phone", data.pop("phoneNumber"))
-
-    address_proxy = make_address(context, data.pop("address"))
-    context.emit(address_proxy)
-
-    proxy.add("addressEntity", address_proxy)
-    proxy.add("address", address_proxy.caption)
-
-    legalForm = data.pop("legalForm")
-    proxy.add("legalForm", legalForm.pop("code_de"))
-    proxy.add("summary", legalForm.get("legalFormText"))
-
-    for email in data.pop("organizationEmails"):
-        proxy.add("email", email)
-    for url in data.pop("websites"):
-        proxy.add("website", url)
-
-    for person_data in data.pop("legalRepresentatives", []):
-        person = make_person(context, proxy.id, person_data)
-        context.emit(person)
-
-        rel = make_representation(context, person, proxy, person_data.pop("function"))
-        context.emit(rel)
-
-    for person_data in data.pop("namedEmployees", []):
-        person = make_person(context, proxy.id, person_data)
-        context.emit(person)
-
-        rel = context.make("Employment")
-        rel.id = context.make_slug("employment", make_entity_id(person.id, proxy.id))
-        rel.add("employer", proxy)
-        rel.add("employee", person)
-        context.emit(rel)
-
-    for name in data.pop("membershipEntries", []):
-        org = context.make("Organization")
-        org.add("name", name)
-        org.id = context.make_slug("org", make_entity_id(fp(name)))
-        context.emit(org)
-
-        rel = context.make("Membership")
-        rel.id = context.make_slug("membership", make_entity_id(proxy.id, org.id))
-        rel.add("organization", org)
-        rel.add("member", proxy)
-        context.emit(rel)
-
-    return proxy
-
-
 def parse_sidejob(context: Zavod, record: dict[str, Any]):
     proxy_data = record
 
-    politician = parse_record(context, record["mandates"][0]['politician'], EntityType.POLITICIAN)
-    organization = parse_record(context, record['sidejob_organization'], EntityType.ORGANIZATION)
+    politician = parse_record(context, record["mandates"][0]["politician"], EntityType.POLITICIAN)
+    organization = parse_record(context, record["sidejob_organization"], EntityType.ORGANIZATION)
 
     label = record["label"]
 
@@ -196,27 +131,24 @@ def parse_sidejob(context: Zavod, record: dict[str, Any]):
     else:
         # TODO: Use suitable type for other (this doesn't fit in general, e.g. for "Reisekosten")
         proxy = make_employment(context, organization, politician, proxy_data)
+    # TODO: Find other suitable models. What about Interval, Other link or Payment?
 
     # TODO: Where to save income?
     # proxy.add("notes", record.pop("income_level", "Unbekanntes Einkommen"))
-
     return proxy
-    
+
 
 def parse_politician(context: Zavod, record: dict[str, Any]):
     proxy_data = record
     proxy = make_person(context, "", proxy_data)
     proxy.add("notes", "Politiker") # TODO: Is there a better way to mark politicians?
-
-    # TODO: Make party \o/
-
+    # TODO: Make party \o/ and relate it to politician.
     return proxy
 
 
 def parse_organization(context: Zavod, record: dict[str, Any]):
     proxy_data = record
     proxy = make_organization(context, proxy_data)
-
     return proxy
 
 
@@ -236,77 +168,7 @@ def parse_record(context: Zavod, record: dict[str, Any], type: str):
     return proxy
 
 
-# TODO Remove
-def parse_record_old(context: Zavod, record: dict[str, Any]):
-    registerId = record.pop("registerNumber")
-    proxy_data = record.pop("lobbyistIdentity")
-
-    proxy_type = EntityType[proxy_data.pop("identity")]
-
-    if proxy_type == EntityType.NATURAL:
-        proxy = make_person(context, "", proxy_data)
-        proxy.id = context.make_slug(registerId)
-    else:
-        proxy = context.make(proxy_type.value)
-        proxy.id = context.make_slug(registerId)
-        proxy = wrangle_organisazion(context, proxy, proxy_data)
-        proxy.add("keywords", [k.get("de") for k in record.pop("fieldsOfInterest")])
-
-    proxy.add("sourceUrl", record.pop("detailsPageUrl"))
-    proxy.add(
-        "status",
-        "active" if record["account"]["activeLobbyist"] else "inactive",  # noqa
-    )
-    proxy.add("description", record.pop("activityDescription"))
-    activity = record.pop("activity")
-
-    notes = activity.get("de", activity.get("text"))
-    proxy.add("notes", notes)
-
-    context.emit(proxy)
-
-    for item in record.pop("donators"):
-        payer = context.make("LegalEntity")
-        payer.add("name", item["name"])
-        payer.add("address", item.get("location"))
-        payer.id = context.make_slug(
-            "donator", make_entity_id("donator", proxy.id, payer.caption)
-        )
-        context.emit(payer)
-
-        payment = context.make("Payment")
-        payment.add("payer", payer)
-        payment.add("beneficiary", proxy)
-        payment.add("purpose", item.pop("description"))
-        payment.add("programme", item.pop("categoryType"))
-        payment.add("startDate", item.pop("fiscalYearStart"))
-        payment.add("endDate", item.pop("fiscalYearend", None))
-        payment.add("amountEur", item["donationEuro"]["from"])
-        payment.add("amountEur", item["donationEuro"]["to"])
-        payment.id = context.make_slug("payment", make_entity_id(payer.id, proxy.id))
-        context.emit(payment)
-
-    for data in record.pop("clientOrganizations"):
-        org = context.make("Organization")
-        name = data["name"]
-        ident = make_entity_id(fp(name))
-        org.id = context.make_slug("org", ident)
-        org = wrangle_organisazion(context, org, data)
-        context.emit(org)
-
-        rel = make_representation(context, proxy, org)
-        context.emit(rel)
-
-    for data in record.get("clientPersons"):
-        person = make_person(context, proxy.id, data)
-        context.emit(person)
-
-        rel = make_representation(context, proxy, person)
-        context.emit(rel)
-    
-
 def parse(context: Zavod):
-    # TODO: This won't collect all entities. Use options and loop to fetch all.
     sideJobsData = fetchAll("/sidejobs")
     # The API doesn't return related data. 
     # To get the relevant entities and minimize the number of requests,
@@ -320,19 +182,19 @@ def parse(context: Zavod):
 
     if (len(relatedMandateIds) > 0):
         mandatesQueryParams = {
-            'current_on': 'all',
-            'id[in]': '[' + ','.join(str(id) for id in relatedMandateIds) + ']',
+            "current_on": "all",
+            "id[in]": "[" + ",".join(str(id) for id in relatedMandateIds) + "]",
         }
-        mandatesData = fetchAll('/candidacies-mandates', mandatesQueryParams)
+        mandatesData = fetchAll("/candidacies-mandates", mandatesQueryParams)
         relatedPoliticianIds = set()
         for mandate in mandatesData:
-            relatedPoliticianIds.add(mandate['politician']['id'])
+            relatedPoliticianIds.add(mandate["politician"]["id"])
 
         if (len(relatedPoliticianIds) > 0):
             politiciansQueryParams = {
-                'id[in]': '[' + ','.join(str(id) for id in relatedPoliticianIds) + ']',
+                "id[in]": "[" + ",".join(str(id) for id in relatedPoliticianIds) + "]",
             }
-            politiciansData = fetchAll('/politicians', politiciansQueryParams)
+            politiciansData = fetchAll("/politicians", politiciansQueryParams)
             for mandatesRecord in mandatesData:
                 for politiciansRecord in politiciansData:
                     if mandatesRecord["politician"]["id"] == politiciansRecord["id"]:
@@ -342,9 +204,9 @@ def parse(context: Zavod):
     if (len(relatedOrganizationIds) > 0):
         # Load organizations for all sidejobs.
         organizationsQueryParams = {
-            'id[in]': '[' + ','.join(str(id) for id in relatedOrganizationIds) + ']',
+            "id[in]": "[" + ",".join(str(id) for id in relatedOrganizationIds) + "]",
         }
-        organizationsData = fetchAll('/sidejob-organizations', organizationsQueryParams)
+        organizationsData = fetchAll("/sidejob-organizations", organizationsQueryParams)
 
     ix = 0
     for ix, sideJobRecord in enumerate(sideJobsData):
@@ -357,7 +219,7 @@ def parse(context: Zavod):
         if mandatesData:
             # Attach mandates including its politician to sidejob record.
             im = 0
-            for im, mandate in enumerate(sideJobRecord['mandates']):
+            for im, mandate in enumerate(sideJobRecord["mandates"]):
                 for mandateRecord in mandatesData:
                     if mandate["id"] == mandateRecord["id"]:
                         sideJobRecord["mandates"][im] = copy.deepcopy(mandateRecord)
@@ -367,6 +229,7 @@ def parse(context: Zavod):
             context.log.info("Parse sidejob record %d ..." % ix)
     if ix:
         context.log.info("Parsed %d sidejob records." % (ix + 1))
+
 
 def fetchAll(path, queryParams = {}):
     ix = 0
@@ -381,7 +244,7 @@ def fetchAll(path, queryParams = {}):
     while (range_start >= 0):
         ix+1
         queryHash = hash("query")
-        sourceFileSuffix = "_" + "".join(x for x in path if x.isalnum()) + '_' + str(queryHash)
+        sourceFileSuffix = "_" + "".join(x for x in path if x.isalnum()) + "_" + str(queryHash)
         data_fp = context.fetch_resource("source" + sourceFileSuffix + ".json", URL + path + query)
         with open(data_fp) as f:
             data = orjson.loads(f.read())
@@ -400,7 +263,7 @@ def fetchAll(path, queryParams = {}):
 
 
 if __name__ == "__main__":
-    # TODO: Had to change path to metadata.yml to make vscode debugging work. Fix this in launch.json
+    # TODO: Had to change path to datasets/de_abgeordnetenwatch/metadata.yml to make vscode debugging work. Fix this in launch.json
     with init_context("datasets/de_abgeordnetenwatch/metadata.yml") as context:
         context.export_metadata("export/index.json")
         parse(context)
