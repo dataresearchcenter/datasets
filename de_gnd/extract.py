e994ec5a0279c32f1fb28ab33bfb5ca97a65fce2
Simon Wörpel <simon.woerpel@pm.me>
2023-10-14 14:46:54 +0200

    🧑‍💻  (de_gnd) configure extract size via env var

--- de_gnd/extract.py
+++ de_gnd/extract.py
@@ -1,11 +1,17 @@
 import gzip
+import os
 from typing import Any, Generator
 
 import ijson
 from investigraph.model import Context
 
+SIZE = int(os.environ.get("GND_TEST_SIZE", 0))
+
 
 def handle(ctx: Context, *args, **kwargs) -> Generator[dict[str, Any], None, None]:
     with gzip.open(ctx.source.uri) as fh:
-        items = ijson.items(fh, "item.item")
-        yield from items
+        items = enumerate(ijson.items(fh, "item.item"))
+        for ix, item in items:
+            yield item
+            if SIZE and ix > SIZE:
+                break
