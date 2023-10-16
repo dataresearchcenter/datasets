1ed2c09d042b028e1e00047c2d06157d58949a73
Simon Wörpel <simon.woerpel@pm.me>
2023-10-16 11:50:00 +0200

    ⚡️ (de_gnd) sort out unused records already during extract stage

--- de_gnd/extract.py
+++ de_gnd/extract.py
@@ -4,14 +4,35 @@ from typing import Any, Generator
 
 import ijson
 from investigraph.model import Context
+from investigraph.types import Record
 
 SIZE = int(os.environ.get("GND_TEST_SIZE", 0))
 
 
+def get_type(record: Record) -> str:
+    # TODO: Adjust for multiple corporate types
+    if "@type" in record.keys():
+        return record["@type"][0].split("#")[-1]
+
+
+def should_transform(ctx: Context, record: Record) -> bool:
+    type_ = get_type(record)
+    if type_:
+        if ctx.source.name == "legalentity":
+            return True
+        # skip ids with '/about' as this is metadata instead of a new entity
+        if "about" in record["@id"]:
+            return False
+        return type_ != "Family"
+    return False
+
+
 def handle(ctx: Context, *args, **kwargs) -> Generator[dict[str, Any], None, None]:
+    ix = 0
     with gzip.open(ctx.source.uri) as fh:
-        items = enumerate(ijson.items(fh, "item.item"))
-        for ix, item in items:
-            yield item
-            if SIZE and ix > SIZE:
-                break
+        for record in ijson.items(fh, "item.item"):
+            if should_transform(ctx, record):
+                yield record
+                ix += 1
+                if SIZE and ix > SIZE:
+                    break
