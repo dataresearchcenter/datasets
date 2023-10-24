0f119ec4f4db0972753b0227ce468a234817cb10
tilana <natalie_widmann@posteo.net>
2023-10-24 13:14:26 +0200

    feat: get data from uri

--- de_lobbypedia_parteispenden/extract.py
+++ de_lobbypedia_parteispenden/extract.py
@@ -1,11 +1,11 @@
 from typing import Any, Generator
+import requests
 
-import json
 from investigraph.model import Context
 
 
 def handle(ctx: Context, *args, **kwargs) -> Generator[dict[str, Any], None, None]:
-    with open(ctx.source.uri, "r") as fh:
-        data = json.load(fh)
-        for key, entry in data["results"].items():
-            yield entry
+    res = requests.get(ctx.source.uri) 
+    data = res.json()['results']
+    for key, entry in data.items():
+        yield entry
