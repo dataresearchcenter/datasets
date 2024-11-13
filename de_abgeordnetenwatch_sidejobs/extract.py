e74365e33d2c55fcdf9ad7d05ac7e6d0ca80d3c5
Simon Wörpel <simon.woerpel@pm.me>
2024-11-13 17:13:23 +0100

    🚧  (de_abgeordnetenwatch_sidejobs) Improve data fetching and transform

--- de_abgeordnetenwatch_sidejobs/extract.py
+++ de_abgeordnetenwatch_sidejobs/extract.py
@@ -1,134 +1,116 @@
-import copy
 import logging
 import os
 import time
+from typing import Iterable
 
-from banal import as_bool
+from banal import as_bool, ensure_dict
 from investigraph import Context
-from investigraph.logic import requests
+from investigraph.logic import fetch
+from investigraph.types import Record, RecordGenerator
+from investigraph.util import dict_merge
 
-URL = "https://www.abgeordnetenwatch.de/api/v2"
+URL = "https://www.abgeordnetenwatch.de/api/v2/"
 TESTING = as_bool(os.environ.get("TESTING"))
+MAX_ERRORS = 5
+CHUNK_SIZE = 500
 
 log = logging.getLogger(__name__)
 
 
-def make_request(*args, **kwargs):
+def make_request(*args, **kwargs) -> Record:
     backoff = 5
-    while True:
+    errors = 0
+    res = None
+    while errors < MAX_ERRORS:
         try:
-            res = requests.get(*args, **kwargs)
+            res = fetch.get(*args, **kwargs)
             return res.json()
-        except Exception:
-            log.warn(
-                "API %s [%d], backoff %d sec." % (args[0], res.status_code, backoff)
+        except Exception as e:
+            log.warning(
+                "API %s [%s], backoff %d sec."
+                % (args[0], res.status_code if res is not None else e, backoff)
             )
+            errors += 1
             time.sleep(backoff)
             backoff = backoff * 2  # gosh
+    raise Exception("Service unavailable for url `{args[0]}`")
 
 
-def fetchByIds(path, ids={}, queryParams={}):
-    # Fetch by IDs using chunks/batch to prevent too long URLs or bad gateway response.
-    chunkSize = 100  # maximum number of IDs to request without failure
-    data = []
-    for idChunk in [
-        list(ids)[i : i + chunkSize] for i in range(0, len(ids), chunkSize)
-    ]:
-        queryParams["id[in]"] = "[" + ",".join(str(id) for id in idChunk) + "]"
-        data += fetchAll(path, queryParams)
-        if TESTING:
-            return data
+def fetch_iter(
+    url: str,
+    range_start: int = 0,
+    chunk_size: int = CHUNK_SIZE,
+) -> RecordGenerator:
+    params = {"range_start": range_start, "range_end": chunk_size}
+    res = make_request(url, params=params)
+    yield from res["data"]
+    if TESTING:
+        return
+    if res["meta"]["result"]["total"] > range_start + chunk_size:
+        yield from fetch_iter(url, range_start + chunk_size)
 
-    return data
 
-
-def fetchAll(path, queryParams={}):
-    ix = 0
-    range_start = 0
-    range_end = 100  # aka limit, default is 100, max. is 1.000
-    dataAll = []
-
-    # Fetch all entities with multiple requests (AW API has limit per request)
-    while range_start >= 0:
-        ix + 1
-        queryParams["range_start"] = range_start
-        queryParams["range_end"] = range_end
-        data = make_request(URL + path, params=queryParams)
-        meta = data["meta"]
-
-        total = int(meta["result"]["total"])
-        range_end = int(meta["result"]["range_end"])
-        if total > (range_start + range_end):
-            range_start = range_start + range_end
-        else:
-            range_start = -1
-
-        dataAll += data["data"]
-
-        if TESTING:
-            return dataAll
-
-    return dataAll
+def fetch_related_items(endpoint: str, ids: Iterable[int], **params) -> RecordGenerator:
+    ids = list(ids)
+    for chunk in [ids[i : i + 100] for i in range(0, len(ids), 100)]:
+        params = {**params, "id[in]": f'[{",".join(str(i) for i in chunk)}]'}
+        res = make_request(URL + endpoint, params=params)
+        yield from res["data"]
 
 
 def handle(ctx: Context, *args, **kwargs):
+    sidejobs: dict[int, Record] = {}
+    mandates: dict[int, Record] = {}
+    politicians: dict[int, Record] = {}
+    organizations: dict[int, Record] = {}
+
     ctx.log.info("Fetching sidejobs ...")
-    sideJobsData = fetchAll("/sidejobs", {"range_end": 1000})
-    ctx.log.info("%d sidejobs" % len(sideJobsData))
-    # The API doesn't return related data.
-    # To get the relevant entities and minimize the number of requests,
-    # we collect their IDs, load them and merge the data before parsing.
-    relatedMandateIds = set()
-    relatedOrganizationIds = set()
-    for sideJob in sideJobsData:
-        for mandate in sideJob["mandates"]:
-            relatedMandateIds.add(mandate["id"])
-        if sideJob["sidejob_organization"]:
-            relatedOrganizationIds.add(sideJob["sidejob_organization"]["id"])
+    for ix, sidejob in enumerate(fetch_iter(URL + "sidejobs"), 1):
+        sidejobs[sidejob["id"]] = sidejob
+        for mandate in sidejob["mandates"]:
+            mandates[mandate["id"]] = mandate
+        organization = ensure_dict(sidejob.get("sidejob_organization"))
+        if organization:
+            organizations[organization["id"]] = organization
+        if TESTING and ix == 100:
+            break
+    ctx.log.info(f"Fetched `{len(sidejobs)}` sidejobs.")
 
-    if len(relatedMandateIds) > 0:
-        ctx.log.info("Fetching mandates ...")
-        mandatesData = fetchByIds(
-            "/candidacies-mandates", relatedMandateIds, {"current_on": "all"}
+    ctx.log.info(f"Fetching `{len(mandates)}` related mandates ...")
+    for ix, mandate in enumerate(
+        fetch_related_items("candidacies-mandates", mandates.keys(), current_on="all"),
+        1,
+    ):
+        mandates[mandate["id"]] = dict_merge(mandates[mandate["id"]], mandate)
+        politicians[mandate["politician"]["id"]] = mandate["politician"]
+        if TESTING and ix == 100:
+            break
+
+    ctx.log.info(f"Fetching `{len(politicians)}` related politicians ...")
+    for ix, politician in enumerate(
+        fetch_related_items("politicians", politicians.keys()), 1
+    ):
+        politicians[politician["id"]] = dict_merge(
+            politicians[politician["id"]], politician
         )
-        ctx.log.info("%d mandates" % len(mandatesData))
-        relatedPoliticianIds = set()
-        for mandate in mandatesData:
-            relatedPoliticianIds.add(mandate["politician"]["id"])
+        if TESTING and ix == 100:
+            break
 
-        if len(relatedPoliticianIds) > 0:
-            ctx.log.info("Fetching related politicians")
-            politiciansData = fetchByIds("/politicians", relatedPoliticianIds)
-            ctx.log.info("%d related politicians" % len(politiciansData))
-            for mandatesRecord in mandatesData:
-                for politiciansRecord in politiciansData:
-                    if mandatesRecord["politician"]["id"] == politiciansRecord["id"]:
-                        mandatesRecord["politician"] = copy.deepcopy(politiciansRecord)
-                        break
+    ctx.log.info(f"Fetching `{len(organizations)}` related organizations ...")
+    for ix, organization in enumerate(
+        fetch_related_items("sidejob-organizations", organizations.keys()), 1
+    ):
+        organizations[organization["id"]] = dict_merge(
+            organizations[organization["id"]], organization
+        )
+        if TESTING and ix == 100:
+            break
 
-    if len(relatedOrganizationIds) > 0:
-        ctx.log.info("Fetching organizations ...")
-        organizationsData = fetchByIds("/sidejob-organizations", relatedOrganizationIds)
-        ctx.log.info("%d organizations" % len(organizationsData))
-
-    for sideJobRecord in sideJobsData:
-        if sideJobRecord["sidejob_organization"] and organizationsData:
-            # Attach organization to sidejob record.
-            for organizationRecord in organizationsData:
-                if (
-                    sideJobRecord["sidejob_organization"]["id"]
-                    == organizationRecord["id"]  # noqa
-                ):
-                    sideJobRecord["sidejob_organization"] = copy.deepcopy(
-                        organizationRecord
-                    )
-                    break
-        if mandatesData:
-            # Attach mandates including its politician to sidejob record.
-            for im, mandate in enumerate(sideJobRecord["mandates"]):
-                for mandateRecord in mandatesData:
-                    if mandate["id"] == mandateRecord["id"]:
-                        sideJobRecord["mandates"][im] = copy.deepcopy(mandateRecord)
-                        break
-
-        yield sideJobRecord
+    yield from politicians.values()
+    yield from organizations.values()
+    # yield from mandates.values()
+    for sidejob in sidejobs.values():
+        sidejob["mandates"] = [
+            dict_merge(m, mandates[m["id"]]) for m in sidejob["mandates"]
+        ]
+        yield sidejob
--- de_abgeordnetenwatch_sidejobs/transform.py
+++ de_abgeordnetenwatch_sidejobs/transform.py
@@ -1,32 +1,75 @@
 import re
-from enum import Enum
-from typing import Any
+from dateparser import parse as _parse_date
+from enum import StrEnum
+from typing import Any, Tuple
 
 from followthemoney.util import make_entity_id
 from ftmq.util import get_country_code
 from investigraph import Context
-from investigraph.types import CE, CEGenerator
-from investigraph.util import fingerprint as fp
+from investigraph.types import CE, CEGenerator, Record
+from investigraph.util import join_text, make_fingerprint_id
 
 
-class EntityType(Enum):
-    SIDEJOB = "Sidejob"
-    POLITICIAN = "Politician"
-    ORGANIZATION = "Organization"
-    PARTY = "Party"
+class EntityType(StrEnum):
+    SIDEJOB = "sidejob"
+    POLITICIAN = "politician"
+    ORGANIZATION = "sidejob_organization"
+    MANDATE = "candidacy_mandate"
+    PARTY = "party"
 
 
-def extract_year(value: str) -> str | None:
+def parse_date(value: str | None) -> str | None:
+    if not value:
+        return
+    value = value.strip()
+    if len(value) == 4:
+        return value
+    date = _parse_date(value, locales=["de"])
+    if date is not None:
+        date = date.isoformat()
+        # only month
+        if sum(c.isdigit() for c in value) == 4:
+            return date[:7]
+        return date[:10]
+
+
+def extract_date_range(value: str) -> Tuple[str | None, str | None]:
     if value:
+        # 'ab 29.11.2023'
+        # 'ab Dezember 2022'
+        # 'ab 2022'
+        m = re.match(r".*(ab|von)\s([\w\s]+)\).*", str(value))
+        if m is not None:
+            return parse_date(m.groups()[1]), None
+
+        # 'bis 29.11.2023'
+        # 'bis April 2022'
+        # 'bis 2022'
+        m = re.match(r".*bis\s([\w\s]+)\).*", str(value))
+        if m is not None:
+            return None, parse_date(m.groups()[0])
+
+        # 'Einkommen im Jahr 2024'
         m = re.match(r".*(\d{4}).*", str(value))
         if m is not None:
             for year in m.groups():
-                return year
+                return year, year
+        # '(Bundestag 2021 - 2025)''
+        m = re.match(r".*(\d{4})\s?-\s?(\d{4}).*", str(value))
+        if m is not None:
+            return m.groups()[0], m.groups()[1]
+    return None, None
 
 
-def make_link(
-    context: Context, politician: CE, organization: CE, data: dict[str, Any]
-) -> CE:
+def extract_date_ranges(*values: str) -> Tuple[str | None, str | None]:
+    for value in values:
+        res = extract_date_range(value)
+        if res[0] or res[1] is not None:
+            return res
+    return None, None
+
+
+def make_link(context: Context, politician: CE, organization: CE, data: Record) -> CE:
     rel = context.make("UnknownLink")
     rel.id = context.make_slug("sidejob", data.pop("id"))
 
@@ -34,16 +77,14 @@ def make_link(
     rel.add("subject", politician)
     rel.add("object", organization)
     rel.add("role", label)
-
     return rel
 
 
 def make_directorship(
-    context: Context, director: CE, organization: CE, data: dict[str, Any]
+    context: Context, director: CE, organization: CE, data: Record
 ) -> CE:
     rel = context.make("Directorship")
     rel.id = context.make_slug("sidejob", data.pop("id"))
-
     label = data.pop("label")
     jobTitleExtra = data.pop("job_title_extra")
     additionalInformation = data.pop("additional_information")
@@ -54,13 +95,10 @@ def make_directorship(
     rel.add("description", jobTitleExtra)
     rel.add("modifiedAt", dataChangeDate)
     rel.add("summary", additionalInformation)
-
     return rel
 
 
-def make_membership(
-    context: Context, member: CE, organization: CE, data: dict[str, Any]
-) -> CE:
+def make_membership(context: Context, member: CE, organization: CE, data: Record) -> CE:
     rel = context.make("Membership")
     rel.id = context.make_slug("membership", make_entity_id(member.id, organization.id))
 
@@ -69,7 +107,6 @@ def make_membership(
         label = data.pop("label")
         description = data.pop("job_title_extra")
         rel.add("role", label)
-        rel.add("date", extract_year(description))
         rel.add("description", description)
         rel.add("modifiedAt", data.pop("data_change_date"))
         rel.add("summary", data.pop("additional_information"))
@@ -79,53 +116,51 @@ def make_membership(
     return rel
 
 
-def make_address(context: Context, data: dict[str, Any]) -> CE:
-    proxy = context.make("Address")
+def make_address(context: Context, data: Record) -> CE:
     country = data["field_country"].pop("label")
-    proxy.add("country", country)
-    if data["field_city"]:
-        city = data["field_city"].pop("label")
-        proxy.add("city", city)
-        proxy.add("full", ", ".join((city, country)))
-    else:
-        proxy.add("full", country)
+    city = None
+    if data.get("field_city"):
+        city = data.pop("field_city")["label"]
+    full = join_text(city, country, sep=", ")
+    proxy = context.make("Address")
     proxy.id = context.make_slug(
         get_country_code(country) or "xx",
-        make_entity_id(fp(proxy.caption)),
+        make_fingerprint_id(full),
         prefix="addr",
     )
+    proxy.add("full", full)
+    proxy.add("country", country)
+    proxy.add("city", city)
     return proxy
 
 
-def make_politician(context: Context, data: dict[str, Any]) -> CE:
+def make_politician(context: Context, data: Record) -> CE:
     proxy = context.make("Person")
     proxy.id = context.make_slug("person", data.pop("id"))
-
-    title = data.pop("field_title", None)
-    firstName = data.pop("first_name")
-    lastName = data.pop("last_name")
-    if title is not None:
-        proxy.add("name", f"{title} {firstName} {lastName}")
-        proxy.add("title", title)
-    else:
-        proxy.add("name", f"{firstName} {lastName}")
-
+    title = data.get("field_title")
+    firstName = data.get("first_name")
+    lastName = data.get("last_name")
+    proxy.add("name", join_text(title, firstName, lastName))
+    proxy.add("title", title)
     proxy.add("topics", "role.pep")
     proxy.add("firstName", firstName)
     proxy.add("lastName", lastName)
-    proxy.add("phone", data.pop("phoneNumber", None))
+    proxy.add("phone", data.get("phoneNumber"))
     for email in data.pop("organizationMemberEmails", []):
         proxy.add("email", email)
+    proxy.add("sourceUrl", data.get("api_url"))
+    proxy.add("gender", data.get("sex"))
+    proxy.add("birthDate", data.get("year_of_birth"))
+    proxy.add("education", data.get("education"))
+    proxy.add("wikidataId", data.get("qid_wikidata"))
     return proxy
 
 
-def make_organization(
-    context: Context, data: dict[str, Any], entity_type: EntityType
-) -> CE:
+def make_organization(context: Context, data: Record, entity_type: EntityType) -> CE:
     proxy = context.make("Organization")
     proxy.id = context.make_slug("organization", data.pop("id"))
-
     proxy.add("name", data.pop("label"))
+    proxy.add("sourceUrl", data.pop("api_url"))
 
     if entity_type == EntityType.PARTY:
         proxy.add("topics", "pol.party")
@@ -143,72 +178,78 @@ def make_organization(
     return proxy
 
 
-def parse_sidejob(context: Context, record: dict[str, Any]):
-    politician = parse_record(
-        context, record["mandates"][0]["politician"], EntityType.POLITICIAN
-    )
-    if record["sidejob_organization"]:
-        organization = parse_record(
-            context, record["sidejob_organization"], EntityType.ORGANIZATION
-        )
+def parse_sidejob(context: Context, record: Record) -> CEGenerator:
+    for mandate in record["mandates"]:
+        politician_data = mandate.get("politician")
+        if politician_data:
+            politician = make_politician(context, politician_data)
+            yield politician
+            if record["sidejob_organization"]:
+                if record["sidejob_organization"].get("id"):
+                    organization = make_organization(
+                        context, record["sidejob_organization"], EntityType.ORGANIZATION
+                    )
+                    yield organization
 
-        label = record["label"]
-        # Choose proxy type by record label.
-        # TODO: Find a more reliable way.
-        if label.startswith("Mitglied"):
-            proxy = make_membership(context, politician, organization, record)
-        elif label.startswith(
-            (
-                "Fraktionsvorsitzender",
-                "Vorsitzende",
-                "Vorsitzender",
-                "Vorstand",
-                "Stellv.",
-                "Erste Vorsitzende",
-                "Erster Vorsitzender",
-            )
-        ):
-            proxy = make_directorship(context, politician, organization, record)
-        else:
-            # TODO: Use suitable type for other (this doesn't fit in general, e.g. for "Beteiligung" or "Reisekosten")
-            proxy = make_link(context, politician, organization, record)
-        # TODO: Find other suitable models. What about Interval, Other link or Payment?
+                    label = record["label"]
+                    start_date, end_date = extract_date_ranges(
+                        label, record["job_title_extra"], mandate["label"]
+                    )
+                    # Choose proxy type by record label.
+                    # TODO: Find a more reliable way.
+                    if label.startswith("Mitglied"):
+                        proxy = make_membership(
+                            context, politician, organization, record
+                        )
+                    elif label.startswith(
+                        (
+                            "Fraktionsvorsitzender",
+                            "Vorsitzende",
+                            "Vorsitzender",
+                            "Vorstand",
+                            "Stellv.",
+                            "Erste Vorsitzende",
+                            "Erster Vorsitzender",
+                        )
+                    ):
+                        proxy = make_directorship(
+                            context, politician, organization, record
+                        )
+                    else:
+                        # TODO: Use suitable type for other (this doesn't fit in general, e.g. for "Beteiligung" or "Reisekosten")
+                        proxy = make_link(context, politician, organization, record)
+                    # TODO: Find other suitable models. What about Interval, Other link or Payment?
 
-        # TODO: Where to save income?
-        # proxy.add("notes", record.pop("income_level", "Unbekanntes Einkommen"))
-    else:
-        # TODO: Handle sidejobs with no organization.
-        proxy = None
-    return proxy
+                    # TODO: Where to save income?
+                    income_level = record.get("income_level") or "Unbekanntes Einkommen"
+                    proxy.add("indexText", f"income_level#{income_level}")
+                    income = record.get("income")
+                    if income:
+                        proxy.add("indexText", f"income#{income}")
+                    proxy.add("startDate", start_date)
+                    proxy.add("endDate", end_date)
+                    proxy.add("sourceUrl", record.pop("api_url"))
+                    yield proxy
+                else:
+                    context.log.warning("No ID for `sidejob_organization`")
+    # TODO: Handle sidejobs with no organization.
+    return
 
 
-def parse_politician(context: Context, record: dict[str, Any]):
+def parse_politician(context: Context, record: Record) -> CEGenerator:
     proxy = make_politician(context, record)
     party = make_organization(context, record["party"], EntityType.PARTY)
-    membership = make_membership(context, proxy, party, None)
-    context.emit(membership)
-    return proxy
-
-
-def parse_record(context: Context, record: dict[str, Any], entity_type: EntityType):
-    if entity_type == EntityType.SIDEJOB:
-        proxy = parse_sidejob(context, record)
-    elif entity_type == EntityType.POLITICIAN:
-        proxy = parse_politician(context, record)
-    elif entity_type == EntityType.ORGANIZATION or entity_type == EntityType.PARTY:
-        proxy = make_organization(context, record, entity_type)
-
-    if proxy:
-        if not proxy.id and record["id"]:
-            proxy.id = context.make_slug(record["id"])
-        proxy.add("sourceUrl", record.pop("api_url"))
-        # TODO: publisher?
-        # TODO: publisherUrl?
-        context.emit(proxy)
-    return proxy
+    yield proxy
+    yield party
+    yield make_membership(context, proxy, party, {})
 
 
 def handle(ctx: Context, record: dict[str, Any], ix: int) -> CEGenerator:
     tx = ctx.task()
-    parse_record(tx, record, EntityType.SIDEJOB)
-    return tx
+    if record["entity_type"] == EntityType.POLITICIAN:
+        yield from parse_politician(tx, record)
+    if record["entity_type"] == EntityType.ORGANIZATION:
+        yield make_organization(tx, record, EntityType.ORGANIZATION)
+    if record["entity_type"] == EntityType.SIDEJOB:
+        yield from parse_sidejob(tx, record)
+    yield from tx
