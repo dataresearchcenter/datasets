8ba4df8cabf79ae1bb98fdd634e06e5e4da5f489
Simon Wörpel <simon.woerpel@pm.me>
2024-11-13 20:02:24 +0100

    ⚡️ (de_abgeordnetenwatch_full) Rewrite extract stage

--- de_abgeordnetenwatch_full/extract.py
+++ de_abgeordnetenwatch_full/extract.py
@@ -1,84 +1,84 @@
-# https://github.com/opensanctions/opensanctions/blob/main/datasets/de/abgeordnetenwatch/crawler.py
+import logging
+import os
+import time
 
-from typing import Any
-import math
-
-from banal import ensure_dict
-from investigraph import Context, Resolver
+from banal import as_bool
+from investigraph import Context
 from investigraph.logic import fetch
-from investigraph.types import RecordGenerator
+from investigraph.types import Record, RecordGenerator
+from investigraph.util import dict_merge
+
+URL = "https://www.abgeordnetenwatch.de/api/v2/"
+TESTING = as_bool(os.environ.get("TESTING"))
+MAX_ERRORS = 5
+CHUNK_SIZE = 1000
+
+log = logging.getLogger(__name__)
 
 
-def fetch_json(
-    url: str, params: dict[str, Any] | None = None, **kwargs
-) -> dict[str, Any]:
-    res = fetch.get(url, params=ensure_dict(params), url_key_only=True, **kwargs)
-    data = res.json()
-    return data.pop("data")
-
-
-def fetch_json_api(
-    url: str, params: dict[str, Any] | None = None, **kwargs
-) -> dict[str, Any]:
-    res = fetch.get(url, params=ensure_dict(params), url_key_only=True, **kwargs)
-    return res.json()
-
-
-def make_records(context, mandates: list[dict[str, Any]]) -> RecordGenerator:
-    for mandate in mandates:
-        parliament_period = mandate.pop("parliament_period")
-        parliament_period_detail = fetch_json(parliament_period["api_url"])
-
-        # Don't get Members of EU-Parlament
-        if "EU" in parliament_period_detail["parliament"]["label"]:
-            continue
-
-        parliament_detail = fetch_json(
-            parliament_period_detail["parliament"]["api_url"]
-        )
-
-        politician = mandate.pop("politician")
-        politician_fullname = politician.pop("label")
-        context.log.info("Get Politician {} detail".format(politician_fullname))
-        politician_detail = fetch_json(politician.pop("api_url"))
-
-        yield {
-            "mandate": mandate,
-            "parliament": parliament_detail,
-            "period": parliament_period_detail,
-            "politician": politician_detail,
-        }
-
-
-def handle(context: Context, res: Resolver) -> RecordGenerator:
-    api_response = res.get_json()
-    if api_response:
-        yield from make_records(context, api_response.pop("data"))
-
-        total_results = api_response.pop("meta").pop("result").pop("total")
-        num_batches = math.ceil(total_results / 100)
-
-        bi = 0
-        while bi < num_batches:
-            context.log.info(
-                "Get Politicians from range {} to {}".format(bi * 100, (bi + 1) * 100)
+def make_request(*args, **kwargs) -> Record:
+    backoff = 5
+    errors = 0
+    res = None
+    while errors < MAX_ERRORS:
+        try:
+            res = fetch.get(*args, **kwargs)
+            return res.json()
+        except Exception as e:
+            log.warning(
+                "API %s [%s], backoff %d sec."
+                % (args[0], res.status_code if res is not None else e, backoff)
             )
+            errors += 1
+            time.sleep(backoff)
+            backoff = backoff * 2  # gosh
+    raise Exception("Service unavailable for url `{args[0]}`")
 
-            if bi > 0:
-                api_response = fetch_json_api(
-                    context.source.uri, params={"range_start": bi * 100}
-                )
 
-                if api_response:
-                    yield from make_records(context, api_response.pop("data"))
+def fetch_iter(
+    url: str, range_start: int = 0, chunk_size: int = CHUNK_SIZE, **params
+) -> RecordGenerator:
+    params = {**params, "range_start": range_start, "range_end": chunk_size}
+    res = make_request(url, params=params)
+    yield from res["data"]
+    if TESTING:
+        return
+    if res["meta"]["result"]["total"] > range_start + chunk_size:
+        params.pop("range_start")
+        params.pop("range_end")
+        yield from fetch_iter(url, range_start + chunk_size, **params)
 
-                    # We need to always recheck total number of results because the API
-                    # returns a incorrect value in the first request without range_start
-                    batch_total_results = (
-                        api_response.pop("meta").pop("result").pop("total")
-                    )
-                    if batch_total_results != total_results:
-                        num_batches = math.ceil(batch_total_results / 100)
 
-            # Increment batch number
-            bi += 1
+def handle(ctx: Context, *args, **kwargs):
+
+    parliaments: dict[int, Record] = {
+        i["id"]: i for i in fetch_iter(URL + "parliaments")
+    }
+    parliament_periods: dict[int, Record] = {
+        i["id"]: i for i in fetch_iter(URL + "parliament-periods")
+    }
+
+    ix = 0
+    ctx.log.info("Fetching politicians ...")
+    for ix, item in enumerate(fetch_iter(URL + "politicians"), 1):
+        yield item
+        if TESTING and ix == 1000:
+            break
+    ctx.log.info(f"Fetched `{ix}` politicians.")
+
+    ctx.log.info("Fetching mandates ...")
+    for ix, item in enumerate(
+        fetch_iter(URL + "candidacies-mandates", current_on="all"), 1
+    ):
+        item["parliament_period"] = dict_merge(
+            item["parliament_period"],
+            parliament_periods[item["parliament_period"]["id"]],
+        )
+        item["parliament_period"]["parliament"] = dict_merge(
+            item["parliament_period"]["parliament"],
+            parliaments[item["parliament_period"]["parliament"]["id"]],
+        )
+        yield item
+        if TESTING and ix == 1000:
+            break
+    ctx.log.info(f"Fetched `{ix}` mandates.")
--- de_abgeordnetenwatch_full/transform.py
+++ de_abgeordnetenwatch_full/transform.py
@@ -1,8 +1,35 @@
-from typing import Any
-from banal import ensure_dict, ensure_list
-from investigraph.types import CE, CEGenerator
+from enum import StrEnum
 
-from investigraph.model import Context
+from banal import ensure_dict, ensure_list
+from investigraph import Context
+from investigraph.types import CE, CEGenerator, Record
+from investigraph.util import join_text
+
+
+class EntityType(StrEnum):
+    POLITICIAN = "politician"
+    MANDATE = "candidacy_mandate"
+
+
+def make_politician(context: Context, data: Record) -> CE:
+    proxy = context.make("Person")
+    proxy.id = context.make_slug("person", data.pop("id"))
+    title = data.get("field_title")
+    firstName = data.get("first_name")
+    lastName = data.get("last_name")
+    proxy.add("name", join_text(title, firstName, lastName))
+    proxy.add("title", title)
+    proxy.add("topics", "role.pep")
+    proxy.add("firstName", firstName)
+    proxy.add("lastName", lastName)
+    proxy.add("phone", data.get("phoneNumber"))
+    proxy.add("sourceUrl", data.get("api_url"))
+    proxy.add("sourceUrl", data.get("abgeordnetenwatch_url"))
+    proxy.add("gender", data.get("sex"))
+    proxy.add("birthDate", data.get("year_of_birth"))
+    proxy.add("education", data.get("education"))
+    proxy.add("wikidataId", data.get("qid_wikidata"))
+    return proxy
 
 
 def make_party(context: Context, name: str) -> CE:
@@ -27,9 +54,7 @@ def make_party_membership(context, person: CE, party_name: str) -> CEGenerator:
     yield make_membership(context, person, party)
 
 
-def make_fraction_membership(
-    context: Context, person: CE, data: dict[str, Any]
-) -> CEGenerator:
+def make_fraction_membership(context: Context, person: CE, data: Record) -> CEGenerator:
     fraction = data.pop("fraction")
     party = make_party(context, fraction.pop("label"))
     party.add("legalForm", fraction.pop("entity_type"))
@@ -83,13 +108,10 @@ def make_occupancy(
     return occupancy
 
 
-def handle(context: Context, record: dict[str, Any], ix: int) -> CEGenerator:
-    parliament_period_detail = record.pop("period")
-    parliament_detail = record.pop("parliament")
-    politician_detail = record.pop("politician")
-    mandate = record.pop("mandate")
-
-    parliament_label = parliament_period_detail["parliament"]["label"]
+def make_mandate(context: Context, record: Record, politician: CE) -> CEGenerator:
+    parliament_period_detail = record.pop("parliament_period")
+    parliament_detail = parliament_period_detail.pop("parliament")
+    parliament_label = parliament_detail["label"]
     parliament_label_long = parliament_detail["label_external_long"]
 
     # Create position
@@ -102,41 +124,36 @@ def handle(context: Context, record: dict[str, Any], ix: int) -> CEGenerator:
         ),
     )
 
-    person = context.make("Person")
-
-    politician_wikidata_id = politician_detail.pop("qid_wikidata")
-    person.id = context.make_slug("person", politician_detail.pop("id"))
-
-    person.add("wikidataId", politician_wikidata_id)
-    person.add("birthDate", politician_detail.pop("year_of_birth"))
-    person.add("name", politician_detail.pop("label"))
-    person.add("firstName", politician_detail.pop("first_name"))
-    person.add("lastName", politician_detail.pop("last_name"))
-    person.add("motherName", politician_detail.pop("birth_name"))
-    person.add("education", politician_detail.pop("education"))
-    person.add("description", politician_detail.pop("occupation"))
-    person.add("sourceUrl", politician_detail.pop("abgeordnetenwatch_url"))
-
-    record_start_date = mandate.pop("start_date") or parliament_period_detail.pop(
+    record_start_date = record.pop("start_date") or parliament_period_detail.pop(
         "start_date_period"
     )
-    record_end_date = mandate.pop("end_date") or parliament_period_detail.pop(
+    record_end_date = record.pop("end_date") or parliament_period_detail.pop(
         "end_date_period"
     )
     occupancy = make_occupancy(
         context,
-        person,
+        politician,
         position,
         start_date=record_start_date,
         end_date=record_end_date,
     )
-    party_name = ensure_dict(politician_detail.pop("party")).get("label")
-    if party_name is not None:
-        yield from make_party_membership(context, person, party_name)
 
-    for membership in ensure_list(mandate.pop("fraction_membership", None)):
-        yield from make_fraction_membership(context, person, membership)
-
-    yield person
     yield position
     yield occupancy
+
+
+def handle(ctx: Context, record: Record, *args, **kwargs) -> CEGenerator:
+    tx = ctx.task()
+    if record["entity_type"] == EntityType.POLITICIAN:
+        politician = make_politician(tx, record)
+        yield politician
+        party_name = ensure_dict(record.pop("party")).get("label")
+        if party_name is not None:
+            yield from make_party_membership(tx, politician, party_name)
+    if record["entity_type"] == EntityType.MANDATE:
+        politician = make_politician(tx, record["politician"])
+        yield politician
+        yield from make_mandate(tx, record, politician)
+        for membership in ensure_list(record.pop("fraction_membership", None)):
+            yield from make_fraction_membership(tx, politician, membership)
+    yield from tx
