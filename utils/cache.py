5f2d1166ae0a459136437b62584bdf877fa3cb64
Simon Wörpel <simon.woerpel@pm.me>
2025-01-06 03:26:57 +0100

    🚧 More cache features

--- utils/cache.py
+++ utils/cache.py
@@ -3,13 +3,28 @@ from typing import Any
 
 import requests
 from anystore.decorators import anycache
+from anystore.store import get_store
 from banal import is_mapping
 from memorious.logic.context import Context
+from servicelayer import env
+
+CACHE_PREFIX = env.get("MEMORIOUS_CACHE_PREFIX", "memorious")
+USE_CACHE = env.to_bool("MEMORIOUS_CACHE", True)
+
+
+def make_cache_key(context: Context, key: str) -> str | None:
+    if not USE_CACHE or not key:
+        return
+    prefix = context.crawler.name
+    return f"{CACHE_PREFIX}/{prefix}/{key}"
 
 
 def make_url_cache_key(
     context: Context, url: str | dict[str, Any] | None, *args, **kwargs
 ) -> str | None:
+    if not USE_CACHE:
+        return
+
     if is_mapping(url):
         url = url["url"]
     if not url:
@@ -20,9 +35,8 @@ def make_url_cache_key(
     if not res.ok:
         return
 
-    prefix = context.crawler.name
     url = url.split("//", 1)[1]
-    return f"{prefix}/{url}"
+    return make_cache_key(context, url)
 
 
 @anycache(key_func=make_url_cache_key)
@@ -31,3 +45,6 @@ def emit_cached(
 ) -> datetime:
     context.emit(rule or "pass", data=data)
     return datetime.now()
+
+
+CACHE = get_store()
