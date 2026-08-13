2d6b3825fe14f72ec0fa9b08e495381f6cfbf04c
Shenya De Silva <shenya@dataresearchcenter.org>
2026-08-13 17:22:37 +0200

    fix: FTS include partial redactions, new payment fields, comma separated amounts

--- datasets/EU/eu_fts/transform.py
+++ datasets/EU/eu_fts/transform.py
@@ -6,6 +6,7 @@ from investigraph.util import clean_name as n
 from investigraph.util import make_data_checksum
 from investigraph.util import make_fingerprint as fp
 from investigraph.util import join_text, make_string_id
+from followthemoney.types import registry
 
 
 def make_address(ctx: SourceContext, record: Record) -> EntityProxy:
@@ -56,10 +57,50 @@ def make_payer(ctx: SourceContext, record: Record) -> EntityProxy | None:
         return proxy
 
 
-def make_payment(ctx: SourceContext, record: Record, beneficiary: EntityProxy) -> EntityProxy:
+def round_two_decimals_comma(ctx: SourceContext, amount: str) -> str:
+    if amount:
+        try:
+            number_obj = registry.number
+            float_amount = number_obj.to_number(amount)
+            rounded = round(float_amount, 2)
+            amount = f"{rounded:,.2f}"
+            # amount = str(round(float_amount, 2))
+        except Exception as e:
+            ctx.log.warn(f"Unable to convert to float: {str(e)} : {amount}")
+            pass
+    return amount
+
+
+def round_two_decimals(ctx: SourceContext, amount: str) -> str:
+    if amount:
+        try:
+            number_obj = registry.number
+            float_amount = number_obj.to_number(amount)
+            amount = str(round(float_amount, 2))
+        except Exception as e:
+            ctx.log.error(f"Unable to convert to float: {str(e)}")
+            pass
+    return amount
+
+
+def make_payment(
+    ctx: SourceContext, record: Record, beneficiary: EntityProxy
+) -> EntityProxy:
     proxy = ctx.make_entity("Payment")
+    redacted = False
     proxy.id = ctx.make_id("payment", beneficiary.id, make_data_checksum(record))
-    amount = record.pop("payment_amount")
+    # amount = Beneficiary’s contracted amount -> commitment consumed amount
+    # amount = record.pop("payment_amount")
+    amount = record.pop("commitment_consumed_amount")
+    if amount is None or amount == "*****":
+        if amount == "*****":
+            redacted = True
+        amount = "0"
+
+    amount = round_two_decimals_comma(ctx, amount)
+    if redacted:
+        amount = "REDACTED"
+
     proxy.add("amountEur", amount)
     proxy.add("amount", amount)
     proxy.add("currency", "EUR")
@@ -67,6 +108,39 @@ def make_payment(ctx: SourceContext, record: Record, beneficiary: EntityProxy) -
     proxy.add("endDate", record["project_endDate"])
     proxy.add("date", record["date"])
     proxy.add("recordId", record.pop("payment_recordId"))
+    # added other values in description
+    contracted_amount = record.get("contracted_amount") or 0
+    commitment_amount = record.get("commitment_contracted_amount") or 0
+    commitment_total = record.get("commitment_total_amount") or 0
+    description = []
+    if contracted_amount == "*****":
+        description.append("Beneficiary’s contracted amount (EUR): REDACTED")
+    else:
+        description.append(
+            f"Beneficiary’s contracted amount (EUR): {round_two_decimals_comma(ctx, contracted_amount)}"
+        )
+    if commitment_amount == "*****":
+        description.append("Commitment contracted amount (EUR): REDACTED")
+    else:
+        description.append(
+            f"Commitment contracted amount (EUR): {round_two_decimals_comma(ctx, commitment_amount)}"
+        )
+    if commitment_total == "*****":
+        description.append("Commitment total amount (EUR): REDACTED")
+    else:
+        description.append(
+            f"Commitment total amount (EUR): {round_two_decimals_comma(ctx, commitment_total)}"
+        )
+
+    description.append(f"Commitment consumed amount (EUR): {amount}")
+
+    # description = [
+    #     f'Beneficiary’s contracted amount (EUR): {round_two_decimals_comma(ctx, contracted_amount)}',
+    #     f'Commitment contracted amount (EUR): {round_two_decimals_comma(ctx, commitment_amount)}',
+    #     f'Commitment total amount (EUR): {round_two_decimals_comma(ctx, commitment_total)}',
+    #     f"Commitment consumed amount (EUR): {amount}",
+    # ]
+    proxy.add("description", description)
     return proxy
 
 
@@ -122,6 +196,9 @@ def make_beneficiary(ctx: SourceContext, record: Record) -> EntityProxy:
         ident = record.pop("beneficiary_vatCode")
         proxy.id = ctx.make_slug(ident.upper())
         proxy.add("vatCode", ident)
+    elif record["beneficiary_vatCode"] == "*****":
+        proxy.id = ctx.make_slug(ident)
+        proxy.add("vatCode", ident)
 
     proxy.add("legalForm", beneficiary_type)
     proxy.add("name", name)
@@ -162,3 +239,43 @@ def handle(ctx: SourceContext, record: Record, ix: int) -> RecordGenerator:
                     ctx, payer, project, record, role="Responsible department"
                 )
         yield payment
+    elif record["beneficiary_name"] == "*****":
+        with open("redacted.txt", "a") as f:
+            f.write(f"{record}\n")
+        checksum = make_data_checksum(record)
+        prefix = checksum[:12]
+        ctx.log.error("redacted record")
+        record["beneficiary_name"] = f"REDACTED_{prefix}"
+        beneficiary = make_beneficiary(ctx, record)
+        address = make_address(ctx, record)
+        project = make_project(ctx, record)
+        payer = make_payer(ctx, record)
+        payment = make_payment(ctx, record, beneficiary)
+
+        beneficiary.add("country", address.first("country"))
+        beneficiary.add("address", address.caption)
+        beneficiary.add("addressEntity", address)
+
+        yield beneficiary
+        yield address
+
+        payment.add("beneficiary", beneficiary)
+
+        if project is not None:
+            yield make_project_participation(ctx, beneficiary, project, record)
+
+            payment.add("project", project)
+            payment.add("purpose", project.caption)
+            yield project
+
+        if payer is not None:
+            payment.add("payer", payer)
+            yield payer
+
+            if project is not None:
+                yield make_project_participation(
+                    ctx, payer, project, record, role="Responsible department"
+                )
+        yield payment
+    else:
+        ctx.log.error("redacted record: not *****")
